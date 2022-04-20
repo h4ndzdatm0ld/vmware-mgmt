7ee3602305c735379764e8147eb54e9f49880344
Hugo Tinoco <43675476+h4ndzdatm0ld@users.noreply.github.com>
2022-04-20 00:23:07 -0700

    Example (#20)
    * tform-make
    
    * Adding example
    
    * plan/apply
    
    Co-authored-by: Hugo Tinoco <htinoco@devpop.localdomain>

---
+++ terraform-plans/awx/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  cloud {
+    organization = "crunchy-org"
+
+    workspaces {
+      name = "awx"
+    }
+  }
+}
+
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "${var.vsphere_cluster}/Resources"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vsphere_vm_template
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.vsphere_vm_name
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = var.vsphere_vm_cpus
+  memory   = var.vsphere_vm_memory
+  guest_id = var.vsphere_vm_guest_id
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = var.vsphere_vm_disk_size
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  }
+  // provisioner "local-exec" {
+  //   command = "ansible-playbook -u ubuntu -i '${self.guest_ip_addresses[0]},' apache-install.yml"
+  // }
+}
+
---
+++ terraform-plans/awx/variables.tf
@@ -0,0 +1,70 @@
+# vSphere Credentials
+
+variable "vsphere_user" {
+  type = string
+  default = "administrator@heshlaw.local"
+}
+
+variable "vsphere_password" {
+  type = string
+}
+
+variable "vsphere_server" {
+  type = string
+  default = "vcenter.heshlaw.local"
+}
+
+## vSphere Infra
+
+variable "vsphere_datastore" {
+  type = string
+  default = "Crunchy"
+}
+
+variable "vsphere_cluster" {
+  type = string
+  default = "HeshLawCluster"
+}
+
+variable "vsphere_network" {
+  type = string
+  default = "CrunchyMgmt"
+}
+
+variable "vsphere_datacenter" {
+  type = string
+  default = "CrunchyDatacenter"
+}
+
+variable "vsphere_vm_name" {
+  type = string
+  default = "crunchy-vm-tf"
+}
+
+
+## VM Vars
+
+variable "vsphere_vm_template" {
+  type = string
+  default = "template-linux-ubuntu-21.10-v22.04"
+}
+
+variable "vsphere_vm_guest_id" {
+  type = string
+  default = "ubuntu64Guest"
+}
+
+variable "vsphere_vm_cpus" {
+  type = number
+  default = 4
+}
+
+variable "vsphere_vm_memory" {
+  type = number
+  default = 4096
+}
+
+variable "vsphere_vm_disk_size" {
+  type = number
+  default = 50
+}
